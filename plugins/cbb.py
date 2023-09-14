9f78f50a29ac9ba648aae02e64b2b1e283293b43
alone-hermit <133490910+alone-hermit@users.noreply.github.com>
2023-09-15 01:32:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/The_Shadow_Knight'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/trippyworld_420'>Click here</a>\n○ Channel : @trippyworld_420\n○ Support Group : @The_Shadow_Knight</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/PLAYBOY_XD'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/PLAYBOY_XD'>Click here</a>\n○ Channel : @PLAYBOY_XD\n○ Support Group : @mullu_hub</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
